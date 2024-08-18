56c04a2af3599934a58cdda8fe1c6896625737f5
Yugsingh05 <yugsingh7622@gmail.com>
2024-08-18 21:48:59 +0530

    second commit

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,10 +3,7 @@ const conf = {
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    // appwriteLikeId: String(import.meta.env.VITE_APPWRITE_LIKE_ID),
-    appwriteUserCollectionId: String(import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID),
-    appwriteSaveCollectionId: String(import.meta.env.VITE_APPWRITE_SAVE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 
 }
 
